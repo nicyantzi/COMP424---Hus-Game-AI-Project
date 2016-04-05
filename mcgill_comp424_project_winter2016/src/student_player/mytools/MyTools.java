9f2a6f350dcc94808f12b859e4eaab55e41ef55c
Nic Yantzi <nic.yantzi@gmail.com>
2016-04-05 00:54:13 -0700

    replaced triple 8s with triple 12s

--- mcgill_comp424_project_winter2016/src/student_player/mytools/MyTools.java
+++ mcgill_comp424_project_winter2016/src/student_player/mytools/MyTools.java
@@ -43,9 +43,9 @@ public class MyTools{
     		//METHOD Choose Policy: Minimax with alpha beta pruning. Then when certain percentage of total game
     		//seeds is reached, use Monte Carlo Search.
         	
-        	int rollout89 = 8;
-        	int rollout79 = 8;
-        	int rollout69 = 8;
+        	int rollout89 = 12;
+        	int rollout79 = 12;
+        	int rollout69 = 12;
     		        	
         	if(greaterThanPer == 1){
     			//System.out.println("Currently have more than 90% of the seeds so using "+ rollout89 + " rollouts.");
